78acfbf03370f2985287cc1a897076eee6c88007
wilx94 <wilson.velez1@upr.edu>
2014-11-10 11:35:38 -0400

    add gui

---
+++ src/Gui.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+public class Gui {
+
+	public static void main(String[] args) {
+		
+		String fn = JOptionPane.showInputDialog("Enter you fn:");
+		String sn = JOptionPane.showInputDialog("enter sn:");
+		
+		int num1 = Integer.parseInt(fn);
+		int num2 = Integer.parseInt(sn);
+		int sum = num1+num2;
+		
+		
+		JOptionPane.showMessageDialog(null, "This are te result"+sum, "Sum", JOptionPane.PLAIN_MESSAGE);
+
+	}
+
+}
