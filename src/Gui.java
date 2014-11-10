6fedb02bfd609064e3a43caf4d0345c3c8869628
wilx94 <wilson.velez1@upr.edu>
2014-11-10 11:43:30 -0400

    add int hola

--- src/Gui.java
+++ src/Gui.java
@@ -9,7 +9,7 @@ public class Gui {
 		int num1 = Integer.parseInt(fn);
 		int num2 = Integer.parseInt(sn);
 		int sum = num1+num2;
-
+int hola;
 		
 		JOptionPane.showMessageDialog(null, "This are te result"+sum, "Sum", JOptionPane.PLAIN_MESSAGE);
 
